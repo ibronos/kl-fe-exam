7982578e8583312572bc47bfa9f97ef77b7b847c
isaidi <ibronos@u-digital.nl>
2023-08-31 16:43:59 +0700

    u

--- src/public/Home.js
+++ src/public/Home.js
@@ -1,7 +1,10 @@
 const Home = () => {
     return (
       <div>
-        <p>home</p>
+        <p className="pb-5">home</p>
+        <a href="https://drive.google.com/drive/folders/1PzcsRB7Xwh2z6VL9XbZBgsGp9e1I8lJk?usp=sharing" className="bg-indigo-500 m-2 p-1 font-bold">
+          download kode program
+        </a>
       </div>
     );
   };
